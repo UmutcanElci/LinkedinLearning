145245df2fbc8ed8d1efd54185cd9a3490310ab2
Umutcan Elçi <umutcanelci@yaani.com>
2023-01-31 18:03:40 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import IntroToDataStructuresAndAlgorithms.LinkedList.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList list = new LinkedList();
+        list.insertAtHead(6);
+        list.insertAtHead(78);
+        list.insertAtHead(5);
+        System.out.println(list.toString());
+        System.out.println(list.length());
+        System.out.println(list.find(5));
     }
 }
