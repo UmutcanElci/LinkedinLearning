19dc87994042b254ba5cbf0f20f7d92cfd3f7f55
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-03 20:35:47 +0300

    BinarySearchTree

---
+++ src/IntroToDataStructuresAndAlgorithms/BinarySearchTrees/BinarySearchTree.java
@@ -0,0 +1,56 @@
+package IntroToDataStructuresAndAlgorithms.BinarySearchTrees;
+
+import com.sun.source.tree.Tree;
+
+public class BinarySearchTree {
+    private TreeNode root;
+
+    public void insert(Integer data){
+        if (root ==null){
+            this.root = new TreeNode(data);
+        }else {
+            root.insert(data);
+        }
+    }
+    public TreeNode find(Integer data){
+        if (root!= null){
+            return root.find(data);
+        }
+        return null;
+    }
+    public void delete(Integer data){
+        TreeNode current = this.root;
+        TreeNode parentt = this.root;
+        boolean isLeftChild = false;
+
+        if (current == null){
+            return;
+        }
+        while (current != null && current.getData() != data){
+            parentt = current;
+            if (data < current.getData()){
+                current = current.getLeftChild();
+                isLeftChild = true;
+            }else {
+                current = current.getLeftChild();
+                isLeftChild = false;
+            }
+        }
+
+        if (current == null){
+            return;
+        }
+        if (current.getLeftChild() == null && current.getRightChild() == null){
+            if (current == root){
+                root = null;
+            }else {
+                if (isLeftChild){
+                    parentt.setLeftChild(null);
+                }else {
+                    parentt.setRightChild(null);
+                }
+            }
+        }
+
+    }
+}
