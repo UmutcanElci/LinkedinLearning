3434e8545d8db4132e1c6b288a0f9e38b5a78a8f
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-03 14:50:38 +0300

    Recursion

---
+++ src/IntroToDataStructuresAndAlgorithms/Recursion/TowerOfHanoi.java
@@ -0,0 +1,20 @@
+package IntroToDataStructuresAndAlgorithms.Recursion;
+
+public class TowerOfHanoi {
+    public void move(int numberOfDiscs, char from,char to,char inter){
+        if (numberOfDiscs == 1){
+            System.out.println("Moving disc "+ numberOfDiscs + " from " + from + " to " + to);
+        }else {
+            move(numberOfDiscs-1,from,inter,to);
+            System.out.println("Moving disc "+ numberOfDiscs + " from " + from + " to " + to);
+            move(numberOfDiscs-1,inter,to,from);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        TowerOfHanoi t = new TowerOfHanoi();
+        t.move(3,'A','B','C');
+    }
+}
