822f496faab8211314b9e1a0c106df83da7ee8fe
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-03 20:55:08 +0300

    Try-Catch-Blocks

--- src/ObjectsAndAPIs/ExceptionHandling/TryAndCatchBlocks.java
+++ src/ObjectsAndAPIs/ExceptionHandling/TryAndCatchBlocks.java
@@ -1,4 +1,27 @@
 package ObjectsAndAPIs.ExceptionHandling;
 
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.IOException;
+
 public class TryAndCatchBlocks {
+    public static void main(String[] args) {
+        File file = new File("Desktop/nonexistent.txt");
+        try{
+            file.createNewFile();
+        }catch (IOException e){
+            System.out.println("Sorry, an error has occurred : "+ e.getMessage());
+            e.printStackTrace();
+        }
+        //Exceptions also have stack traces
+        //Stack trace is an log of method calls that led to a thrown exception
+
+        //Checked and Unchecked Exeptions
+        //Checked Exceptions
+        //Exceptions that are verified by the compiler
+        //Unchecked Eceptions
+        //Exceptions that are not verified by the compiler
+
+
+    }
 }
