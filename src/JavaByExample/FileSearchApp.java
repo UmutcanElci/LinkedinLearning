9abb0f2428d1c59ed145b643136e05b88724568c
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-07 22:28:20 +0300

    File Search App

---
+++ src/JavaByExample/FileSearchApp.java
@@ -0,0 +1,33 @@
+package JavaByExample;
+public class FileSearchApp {
+    String path;
+    String regex;
+    String zipFileName;
+    public static void main(String[] args) {
+
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    public String getZipFileName() {
+        return zipFileName;
+    }
+
+    public void setZipFileName(String zipFileName) {
+        this.zipFileName = zipFileName;
+    }
+}
