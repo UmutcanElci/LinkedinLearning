ef6c22705e21e47ba67c174bded4601d0e0a7285
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-07 22:47:24 +0300

    File Search App

--- src/JavaByExample/FileSearchApp.java
+++ src/JavaByExample/FileSearchApp.java
@@ -1,10 +1,39 @@
 package JavaByExample;
+
+import java.io.File;
+
 public class FileSearchApp {
     String path;
     String regex;
     String zipFileName;
     public static void main(String[] args) {
+        FileSearchApp app =new FileSearchApp();
+        switch (Math.min(args.length,3)){
+            case 0:
+                System.out.println("USAGE: FileSearchApp path [regex] [zip file]");
+                return;
+            case 3: app.setZipFileName(args[2]);
+            case 2: app.setZipFileName(args[1]);
+            case 1: app.setZipFileName(args[0]);
+        }
+        try {
+            app.walkDirectory(app.getPath());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
+    }
+
+    public void walkDirectory(String path){
+        System.out.println("walkDirectory : " + path);
+        searchFile(null);
+        addFileTopZip(null);
+    }
+    public void searchFile(File file){
+        System.out.println("searchFile : "+file);
+    }
+    public void addFileTopZip(File file){
+        System.out.println("addFileToZip : "+ file);
     }
 
     public String getPath() {
