09611f7685bce0023a0472dc995cbff42447ee2c
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-08 22:21:05 +0300

    Challenges-LinkedinLearning

---
+++ src/CodingChallengesJava/Challenges.java
@@ -0,0 +1,50 @@
+package CodingChallengesJava;
+
+import java.util.Scanner;
+
+public class Challenges {
+    public static boolean isEven(int n){
+        return n % 2 == 0;
+
+        /*
+        if (n % 2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+
+         */
+    }
+
+    public static boolean isPasswordComplex(String password){
+        /*
+        Must have at least six characters
+        Must have one uppercase letter
+        Must have one lowercase letter
+        Must have one number
+         */
+        if (password.length()<6){
+            return false;
+        }
+        boolean hasLowercaseLetter = false;
+        boolean hasUppercaseLetter = false;
+        boolean hasNumber = false;
+
+        for (int i = 0; i<password.length() || !hasLowercaseLetter && !hasUppercaseLetter && ! hasNumber; i++){
+            char current = password.charAt(i);
+            if (Character.isDigit(current)){
+                hasNumber = true;
+            }else if(Character.isUpperCase(current)){
+                hasUppercaseLetter = true;
+            } else if (Character.isLowerCase(current)) {
+                hasLowercaseLetter = true;
+            }
+        }
+        return hasNumber && hasLowercaseLetter && hasUppercaseLetter;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
