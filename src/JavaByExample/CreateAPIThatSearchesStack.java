925e44b59dace1b2a458e3ae809c4f824448a5e1
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-10 01:25:23 +0300

    StudentManagement

---
+++ src/JavaByExample/CreateAPIThatSearchesStack.java
@@ -0,0 +1,8 @@
+package JavaByExample;
+
+public class CreateAPIThatSearchesStack {
+    /*
+    API is code that other code can use
+    Every time you write a class with public methods you're creating API
+     */
+}
