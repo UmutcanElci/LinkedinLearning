ed80dc2085298dcc49579e92d6bd7e84a5dc6448
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-10 17:10:13 +0300

    ArrayLinkedinLearning

---
+++ src/CodingChallengesJava/PracticeItArray/PracticeArray.java
@@ -0,0 +1,38 @@
+package CodingChallengesJava.PracticeItArray;
+
+import java.util.Arrays;
+
+public class PracticeArray {
+    public static void main(String[] args) {
+        //double[] lotteryNums = new double[5];//{45,92,33,21,52}
+        //Iterating through arrays
+        /*
+        int[] arr = new int[] {92,12,38,74};
+        for (int i = 0 ; i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+
+        for (int i = arr.length-1; i>=0;i--){
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+
+        for (int item : arr){
+            System.out.println(item);
+        }
+        System.out.println();
+
+        //Java Streams
+        Arrays.stream(arr).forEach(System.out::println);
+        //Arrays.stream(arr).filter();
+        //Arrays.stream(arr).sum();
+
+         */
+
+
+        //Find Second-Smallest item in an array
+
+        
+    }
+}
